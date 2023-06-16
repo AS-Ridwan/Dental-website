f57b1b47e7b605a615dc5ec1549238c9900d155c
hipopridwan <ridwanhipop@gmail.com>
2023-06-16 16:44:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
